ba938bbb1faf748f9fc317314407dc4c88fe307d
ThePhaseCat <alexmgamer10@gmail.com>
2022-05-22 09:35:04 -0600

    hi

---
+++ src/numberTest.java
@@ -0,0 +1,19 @@
+public class numberTest {
+
+    public static void main(String[] args) {
+        int i = 10;
+        System.out.println(i++);
+        System.out.println(--i);
+        --i;
+        i--;
+        System.out.println(i);
+        System.out.println(++i);
+        System.out.println(i--);
+        System.out.println(i);
+        i++;
+        i = i++ + ++i;
+        System.out.println(i);
+        i = i++ + i++;
+        System.out.println(i);
+    }
+}
---
+++ src/preIncrementDemo.java
@@ -0,0 +1,11 @@
+import static java.lang.System.out;
+public class preIncrementDemo {
+    public static void main(String args[]) {
+        int numberOfBunnies = 27;
+
+        numberOfBunnies++;
+        out.println(numberOfBunnies);
+        out.println(numberOfBunnies++);
+        out.println(numberOfBunnies);
+    }
+}
